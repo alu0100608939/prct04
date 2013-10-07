26e31fc588312778a12b139be4c608bb933b19f3
alu0100456608 <alu0100456608@ull.edu.es>
2013-10-07 15:27:41 +0100

    Implementada la multiplicación

--- main.rb
+++ main.rb
@@ -19,6 +19,33 @@ def imprimir(matriz)
    print "\n"
 end
 
+def multiplicar(m1, m2)
+   
+   #Se inicializa la variable temporal para almacenar el resultado
+   res = Array.new($cols) { Array.new($rows) }
+   
+   #Se recorren las filas de m1
+   $rows.times do |i|
+      $cols.times do |j|
+         #Se multiplican cada uno de los elementos de la fila actual de m1 por los de la columna correspondiente de m2
+         #y se acumulan los resultados parciales en tmp.
+         tmp = 0
+         $rows.times do |k|
+            tmp += m1[i][k] * m2[k][j]
+         end
+         #Se guarda el resultado en la fila y columna correspondiente del resultado
+         res[i][j] = tmp
+      end
+   end
+   
+   #Se devuelve el resultado
+   return res
+end
+
 #Se imprimen ambas matrices
+puts "Matriz A:"
 imprimir a
+puts "Matriz B:"
 imprimir b
+puts "Resultado de multiplicar A x B"
+imprimir (multiplicar(a,b))
